7141eb01b773daba792b46b841133dbce5f11b57
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-01 14:59:12 +0200

    test pt.1

---
+++ script.js
@@ -0,0 +1,49 @@
+//HTML CARD
+
+/* <div class="team-card">
+        <div class="card-image">
+                <img
+                    src="img/wayne-barnett-founder-ceo.jpg"
+                    alt="Wayne Barnett"
+                />
+        </div>
+        <div class="card-text">
+                <h3>Wayne Barnett</h3>
+                <p>Founder & CEO</p>
+        </div>
+</div> */
+
+//creo l'array di oggetti contenente i membre del Team
+//per iniziare il numero di membri è pari a 6
+const membri=[
+    //OBJECT 1 ALL'INDICE 0
+    {
+        nominativo: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+    },
+    //OBJECT 2 ALL'INDICE 1
+    {
+        nominativo: "Angela Carol",
+        ruolo: "Chief Editor",
+    },
+    //OBJECT 3 ALL'INDICE 2
+    {
+        nominativo: "Walter Gordon",
+        ruolo: "Office Manager",
+    },
+    //OBJECT 4 ALL'INDICE 3
+    {
+        nominativo: "Angela Lopez",
+        ruolo: "Social Media Manager",
+    },
+    //OBJECT 5 ALL'INDICE 4
+    {
+        nominativo: "Scott Estrada",
+        ruolo: "Developer",
+    },
+    //OBJECT 6 ALL'INDICE 5
+    {
+        nominativo: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+    },
+];
